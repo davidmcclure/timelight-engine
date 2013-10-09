f374e577d0420be038755ae6b23a4d06bcd1c75b
David McClure <david.mcclure@virginia.edu>
2013-10-08 17:24:05 -0700

    Naive pass at segment rendering, partially working.

--- tl-core.coffee
+++ tl-core.coffee
@@ -5,23 +5,32 @@
 class Calendar
 
 
+  ###
+  #
   # Store the unit definition array, track ordering.
   #
   # @param {Array} @units
+  #
+  ###
   constructor: (@units) ->
 
-    # Track the integer offset of each unit.
+    # Cache the integer offset of each unit.
     @order = {}
     for unit, i in @units
       @order[unit[0]] = i
 
 
+  ###
+  #
   # Convert between two units.
   #
   # @param {Number} quantity
   # @param {String} unit1
   # @param {String} unit2
+  #
   # @return {Number}
+  #
+  ###
   convert: (quantity, unit1, unit2) ->
 
     # If no unit change, just return the value.
@@ -48,6 +57,61 @@ class Calendar
     quantity
 
 
+  ###
+  #
+  # Convert a multi-unit position object to  some number a specific unit.
+  # Eg: { years: 1, days: 100 } => 465 days
+  #
+  # @param {Object} position
+  # @param {String} unit
+  #
+  # @return {Number}
+  #
+  ###
+  positionToUnits: (position, unit) ->
+
+    total = 0
+
+    # Convert each position part to the target unit, add to total.
+    for u, q of position
+      total += @convert q, u, unit
+
+    total
+
+
+  ###
+  #
+  # Render a calendar segment.
+  #
+  # @param {Object} center
+  # @param {String} labelUnit
+  # @param {String} tickUnit
+  # @param {Number} unitsPerTick
+  # @param {Number} tickRadius
+  #
+  # @return {Array}
+  #
+  ###
+  render: (center, labelUnit, tickUnit, unitsPerTick, tickRadius) ->
+
+    # First, get the focus in tick units at the left boundary of the segment.
+    focusOffset = @positionToUnits center, tickUnit
+    startOffset = focusOffset - tickRadius*unitsPerTick
+
+    segment = []
+    for i in [0..tickRadius*2]
+
+      # Get the offset in tick and label units at the current tick.
+      offset = startOffset + unitsPerTick*i
+      labelOffset = @convert offset, tickUnit, labelUnit
+
+      # If the label offset is an integer, push it as a marker.
+      segment.push if labelOffset%1 == 0 then labelOffset else "*"
+
+
+    segment.join("|")
+
+
 # TODO|dev
 calendar = new Calendar [
   ["millisecond"],
@@ -55,9 +119,9 @@ calendar = new Calendar [
   ["minute", 60],
   ["hour", 60],
   ["day", 24],
-  ["year", 356],
+  ["year", 365],
   ["decade", 10],
   ["century", 10]
 ]
 
-console.log calendar.convert 1, "year", "hour"
+console.log calendar.render { year: 2013, day: 10 }, "year", "day", 50, 50
--- tl-core.js
+++ tl-core.js
@@ -2,6 +2,14 @@
   var Calendar, calendar;
 
   Calendar = (function() {
+    /*
+    #
+    # Store the unit definition array, track ordering.
+    #
+    # @param {Array} @units
+    #
+    */
+
     function Calendar(units) {
       var i, unit, _i, _len, _ref;
       this.units = units;
@@ -12,6 +20,19 @@
         this.order[unit[0]] = i;
       }
     }
+
+    /*
+    #
+    # Convert between two units.
+    #
+    # @param {Number} quantity
+    # @param {String} unit1
+    # @param {String} unit2
+    #
+    # @return {Number}
+    #
+    */
+
 
     Calendar.prototype.convert = function(quantity, unit1, unit2) {
       var dir, i, i1, i2, _i, _j, _ref;
@@ -34,12 +55,66 @@
       return quantity;
     };
 
+    /*
+    #
+    # Convert a multi-unit position object to  some number a specific unit.
+    # Eg: { years: 1, days: 100 } => 465 days
+    #
+    # @param {Object} position
+    # @param {String} unit
+    #
+    # @return {Number}
+    #
+    */
+
+
+    Calendar.prototype.positionToUnits = function(position, unit) {
+      var q, total, u;
+      total = 0;
+      for (u in position) {
+        q = position[u];
+        total += this.convert(q, u, unit);
+      }
+      return total;
+    };
+
+    /*
+    #
+    # Render a calendar segment.
+    #
+    # @param {Object} center
+    # @param {String} labelUnit
+    # @param {String} tickUnit
+    # @param {Number} unitsPerTick
+    # @param {Number} tickRadius
+    #
+    # @return {Array}
+    #
+    */
+
+
+    Calendar.prototype.render = function(center, labelUnit, tickUnit, unitsPerTick, tickRadius) {
+      var focusOffset, i, labelOffset, offset, segment, startOffset, _i, _ref;
+      focusOffset = this.positionToUnits(center, tickUnit);
+      startOffset = focusOffset - tickRadius * unitsPerTick;
+      segment = [];
+      for (i = _i = 0, _ref = tickRadius * 2; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+        offset = startOffset + unitsPerTick * i;
+        labelOffset = this.convert(offset, tickUnit, labelUnit);
+        segment.push(labelOffset % 1 === 0 ? labelOffset : "*");
+      }
+      return segment.join("|");
+    };
+
     return Calendar;
 
   })();
 
-  calendar = new Calendar([["millisecond"], ["second", 1000], ["minute", 60], ["hour", 60], ["day", 24], ["year", 356], ["decade", 10], ["century", 10]]);
+  calendar = new Calendar([["millisecond"], ["second", 1000], ["minute", 60], ["hour", 60], ["day", 24], ["year", 365], ["decade", 10], ["century", 10]]);
 
-  console.log(calendar.convert(1, "year", "hour"));
+  console.log(calendar.render({
+    year: 2013,
+    day: 10
+  }, "year", "day", 50, 50));
 
 }).call(this);
